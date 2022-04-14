71420c24325f21e81b1ba7810d908f67ee9c1023
unknown <yusufyuldashev2903@gmail.com>
2022-04-14 17:23:57 +0500

    changes

---
+++ src/App.vue
@@ -0,0 +1,37 @@
+<template>
+  <div class="all">
+    <router-view />
+  </div>
+</template>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+}
+
+#nav {
+  padding: 30px;
+}
+.all {
+  background-image: url('./assets/back.jpg') !important;
+  background-repeat: no-repeat;
+  background-size: cover;
+}
+#nav a {
+  font-weight: bold;
+  color: #2c3e50;
+}
+
+#nav a.router-link-exact-active {
+  color: #42b983;
+}
+@media only screen and (max-width: 765px) {
+  .all {
+    background-size: cover;
+  }
+}
+</style>
